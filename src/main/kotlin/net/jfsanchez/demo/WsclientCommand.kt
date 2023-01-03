a5f4d0d13e133732764ad4456f9e0caf4dbaf1bf
Jorge F. Sánchez <jfsanchez91>
2023-01-03 18:08:31 +0000

    Initial commit

---
+++ src/main/kotlin/net/jfsanchez/demo/WsclientCommand.kt
@@ -0,0 +1,30 @@
+package net.jfsanchez.demo
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import io.micronaut.context.ApplicationContext
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name = "wsclient", description = ["..."],
+        mixinStandardHelpOptions = true)
+class WsclientCommand : Runnable {
+
+    @Option(names = ["-v", "--verbose"], description = ["..."])
+    private var verbose : Boolean = false
+
+    override fun run() {
+        // business logic here
+        if (verbose) {
+            println("Hi!")
+        }
+    }
+
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            PicocliRunner.run(WsclientCommand::class.java, *args)
+        }
+    }
+}
